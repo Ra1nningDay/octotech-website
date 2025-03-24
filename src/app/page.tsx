8326a211bb5bf00ce758f49fe64f2f65c21cf482
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-24 13:58:30 +0700

    feat: setup page layout and hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-import Navbar from "@/components/ui/layout/Navbar";
-import Footer from "@/components/ui/layout/footer/Footer";
+import PageLayout from "@/components/ui/layout/PageLayout";
 
 export default function Home() {
     return (
-        <div className="flex flex-col h-screen">
-            <Navbar />
-            <main className="flex-1">
+        <PageLayout>
+            <div className="flex flex-col items-center justify-center h-full min-h-screen">
                 <h1 className="text-4xl font-bold text-center mt-20">
                     Welcome to OctoTech
                 </h1>
-            </main>
-            <Footer />
-        </div>
+            </div>
+        </PageLayout>
     );
 }
---
+++ src/components/ui/layout/PageLayout.tsx
@@ -0,0 +1,19 @@
+// Note: Page layout component
+// Note: This component is used to wrap the page content with the Navbar and Footer components.
+
+import Navbar from "@/components/ui/layout/Navbar";
+import Footer from "@/components/ui/layout/footer/Footer";
+
+interface PageLayoutProps {
+    children: React.ReactNode;
+}
+
+export default function PageLayout({ children }: PageLayoutProps) {
+    return (
+        <div className="flex flex-col h-screen ">
+            <Navbar />
+            <main className="flex-1">{children}</main>
+            <Footer />
+        </div>
+    );
+}
