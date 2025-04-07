da069cf6341991f8171180f6553f36f45cea80f7
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-04-07 16:31:56 +0700

    fix: domains for image optimize

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: ["https://octotechth.netlify.app"],
+    domains: ["octotechth.netlify.app"],
   },
 };
 
